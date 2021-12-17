2ef9b332633da18c67870835e4ef6629f902443f
Herik S. Lorenção <herik.lorencao@itixti.com.br>
2021-12-17 13:22:13 -0300

    adicionando modificador internal

---
+++ ByteBank.Modelos/AutenticacaoHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos
+{
+    // Se não colocar modificador é considerado internal por default
+    internal class AutenticacaoHelper
+    {
+        public bool CompararSenhas(string senhaOriginal, string senhaInformada)
+        {
+            return senhaOriginal == senhaInformada;
+        }
+    }
+}
--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,7 @@ namespace ByteBank.SistemaAgencia
         static void Main(string[] args)
         {
             ContaCorrente contaCorrente = new ContaCorrente(1235, 123256);
+            GerenteDeConta gerente = new GerenteDeConta("124.224.124-12");
 
             Console.ReadLine();
         }
